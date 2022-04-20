7c20aef5d73977510b90a7d711e037f0fae149f4
Avvami <dima.valbu.01@gmail.com>
2022-04-20 12:51:10 +0700

    Initial commit

---
+++ app/src/main/java/com/bignerdranch/android/coursework_movies/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.bignerdranch.android.coursework_movies
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+    }
+
+}
