4ea73666f5aac6ced45c51ef84746f37f27f2fd5
Avvami <dima.valbu.01@gmail.com>
2022-05-15 12:30:19 +0700

    Добавленые фрагменты и переключаения между ними

--- app/src/main/java/com/bignerdranch/android/coursework_movies/MainActivity.kt
+++ app/src/main/java/com/bignerdranch/android/coursework_movies/MainActivity.kt
@@ -2,11 +2,28 @@ package com.bignerdranch.android.coursework_movies
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.viewpager.widget.ViewPager
+import com.bignerdranch.android.coursework_movies.screens.fragments.FavouriteFragment
+import com.bignerdranch.android.coursework_movies.screens.fragments.MoviesFragment
+import com.bignerdranch.android.coursework_movies.screens.fragments.SeriesFragment
+import com.bignerdranch.android.coursework_movies.screens.fragments.adapters.ViewPagerAdapter
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        setOnTabs()
     }
 
+    private fun setOnTabs() {
+        val adapter = ViewPagerAdapter(supportFragmentManager)
+        adapter.addFragment(MoviesFragment(), "ФИЛЬМЫ")
+        adapter.addFragment(SeriesFragment(), "СЕРИАЛЫ")
+        adapter.addFragment(FavouriteFragment(), "МОЙ СПИСОК")
+        viewPager.adapter = adapter
+        tabLayout.setupWithViewPager(viewPager)
+    }
 }
--- app/src/main/java/com/bignerdranch/android/coursework_movies/screens/fragments/MoviesFragment.kt
+++ app/src/main/java/com/bignerdranch/android/coursework_movies/screens/fragments/MoviesFragment.kt
@@ -1,4 +1,4 @@
-package com.bignerdranch.android.coursework_movies.screens.main
+package com.bignerdranch.android.coursework_movies.screens.fragments
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
@@ -7,15 +7,14 @@ import android.view.View
 import android.view.ViewGroup
 import com.bignerdranch.android.coursework_movies.R
 
-
-class MainFragment : Fragment() {
+class MoviesFragment : Fragment() {
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
+        savedInstanceState: Bundle?)
+    :View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_main, container, false)
+        return inflater.inflate(R.layout.fragment_movies, container, false)
     }
 
 }
